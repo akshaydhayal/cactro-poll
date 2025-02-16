7280acf343c60704bfedd2880faf02753fe9e9f3
akshaydhayal <akshaydhayalkd99@gmail.com>
2025-02-16 15:02:47 +0530

    changes

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,6 +1,6 @@
 const Landing = () => {
   return (
-    <div className="flex flex-col justify-center  h-screen px-12 gap-3 w-2/3">
+    <div className="flex flex-col justify-center  px-12 gap-3 w-2/3">
       <p className="text-4xl tracking-wider font-semibold text-[#64b6ff]">Poll App</p>
       <p className="text-slate-400">Ready for polling?</p>
       <p className="text-slate-300 text-lg  mt-2">Just Create a Poll and share it with your friends.</p>
