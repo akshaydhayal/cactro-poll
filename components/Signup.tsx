5eb2ed0532cf39ff1242ea429f79628d3dae1fb3
akshaydhayal <akshaydhayalkd99@gmail.com>
2025-02-16 14:25:05 +0530

    changes

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Input } from "./Input";
 import Button from "./Button";
-// import Google from "./icons/Google";
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -51,9 +50,6 @@ const Signup = () => {
             Register
           </Button>
           <p className="text-slate-300 font-medium text-sm text-center">OR</p>
-          {/* <Button fullWidth={true} icon={<Google />} variant="light" defaultStyle="tracking-wide font-extrabold">
-            Signin with Google
-          </Button> */}
         </div>
         <div className="flex gap-2 justify-center">
           <p className="text-slate-300 text-sm">Already have an account?</p>
